875a91a9ec91aa59a30a68311419788c10e81059
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2021-12-28 22:53:35 -0300

    test: should add user with complete data to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,18 @@
+import { UserData } from './user-date'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', () => {
+    const users: UserData[] = []
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+    //   repo
+    // )
+
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // const response = await usecase.registerUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail(email)
+
+    // expect((await user).name).toBe('any_name')
+  })
+})
