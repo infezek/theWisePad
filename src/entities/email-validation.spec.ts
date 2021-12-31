c91e48d1eb5767c1157d54b00a2c14f74ee7f017
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2021-12-31 20:27:01 -0300

    test: validate null and empty emails

---
+++ src/entities/email-validation.spec.ts
@@ -0,0 +1,13 @@
+import { Email } from './email'
+
+describe('Email validation', () => {
+  test('should not accept null string', () => {
+    const email = null
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept empty strings', () => {
+    const email: string = ''
+    expect(Email.validate(email)).toBeFalsy()
+  })
+})
