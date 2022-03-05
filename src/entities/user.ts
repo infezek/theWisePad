25c4a4b85ac016483239d09c557e4ff58f503974
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-03-05 12:22:17 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,11 +1,19 @@
 import { UserData } from './user-date'
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { Email } from './email'
 import { InvalidNameError } from './errors/invalid-name-error'
 import { Name } from './name'
 
 export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor(name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
   static create(
     userData: UserData
   ): Either<InvalidEmailError | InvalidNameError, User> {
@@ -19,5 +27,10 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
