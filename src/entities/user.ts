266e14a4965e6899e4f9e97c8b97a259b173c591
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-03-05 11:48:13 -0300

    feat: verify name while creating user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -2,9 +2,18 @@ import { UserData } from './user-date'
 import { Either, left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { Email } from './email'
+import { InvalidNameError } from './errors/invalid-name-error'
+import { Name } from './name'
 
 export class User {
-  static create(userData: UserData): Either<InvalidEmailError, User> {
+  static create(
+    userData: UserData
+  ): Either<InvalidEmailError | InvalidNameError, User> {
+    const nameOrError = Name.create(userData.name)
+    if (nameOrError.isLeft()) {
+      return left(new InvalidNameError())
+    }
+
     const emailOrError = Email.create(userData.email)
 
     if (emailOrError.isLeft()) {
