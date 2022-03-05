1cc9b5e4c925f184b767cf3439d4229b0aef96ab
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-03-05 11:49:04 -0300

    test: verify name ehile creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 import { User } from './user'
 
 describe('User domain entity', () => {
@@ -7,5 +8,17 @@ describe('User domain entity', () => {
     const invalidEmail = 'invalid-email'
     const error = User.create({ name: 'John', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  it('should not create user with invalid name (too few characters)', () => {
+    const invalidName = '0           '
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  it('should not create user with invalid name (too few characters)', () => {
+    const invalidName = '0'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
